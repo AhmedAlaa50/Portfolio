b4a882a112ab09173ee6ed85e436f50fa47c2a35
Ahmed Alaa <1808283@eng.asu.edu.eg>
2025-04-30 19:02:23 +0300

    Updated hover for a tags

--- AhmedCodes/src/components/NavBar.tsx
+++ AhmedCodes/src/components/NavBar.tsx
@@ -1,13 +1,16 @@
 import { useState } from 'react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 
+const navBarStyles =
+  "relative px-4 py-1 text-base md:text-xl font-semibold hover:text-custom-yellow transition-colors duration-300 before:content-[''] before:absolute before:inset-0 before:rounded-full before:border-2 before:border-custom-yellow before:opacity-0 hover:before:opacity-100 before:transition-all before:duration-300";
+
 const NavBar: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <nav className="bg-custom-dark px-4 md:px-8 py-4 md:py-6 text-custom-green fixed top-0 left-0 w-full z-50 shadow-lg transition-all duration-300 ease-in-out">
       <div className="flex justify-between items-center">
-        <h1 className="text-2xl md:text-3xl font-bold text-custom-yellow">
+        <h1 className="text-2xl md:text-3xl font-bold text-custom-yellow selection:text-[var(--color-custom-green)]">
           AhmedCodes
         </h1>
 
@@ -27,28 +30,28 @@ const NavBar: React.FC = () => {
         {/* Desktop menu */}
         <ul className="hidden md:flex gap-6 text-base md:text-xl font-semibold">
           <li>
-            <a href="#home" className="hover:underline">
+            <a href="#home" className={navBarStyles}>
               Home
             </a>
           </li>
           <li>
-            <a href="#skills" className="hover:underline">
+            <a href="#skills" className={navBarStyles}>
               Skills
             </a>
           </li>
           <li>
-            <a href="#projects" className="hover:underline">
+            <a href="#projects" className={navBarStyles}>
               Projects
             </a>
           </li>
           <li>
-            <a href="#about" className="hover:underline">
+            <a href="#about" className={navBarStyles}>
               About
             </a>
           </li>
           <li>
-            <a href="#contact" className="hover:underline">
-              Contact
+            <a href="#contact" className={navBarStyles}>
+              Conatct
             </a>
           </li>
         </ul>
