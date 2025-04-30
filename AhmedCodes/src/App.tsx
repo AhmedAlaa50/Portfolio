d48dc6a57bc0c9ec79d89e91cbc57868cb70e2f5
Ahmed Alaa <1808283@eng.asu.edu.eg>
2025-04-30 19:08:03 +0300

    Updated StartingScreen.tsx to fit with arrow and updated App.tsx

--- AhmedCodes/src/App.tsx
+++ AhmedCodes/src/App.tsx
@@ -1,24 +1,24 @@
 import NavBar from './components/NavBar';
 import StartingScreen from './components/StartingScreen';
+import Welcome from './components/Welcome';
 import About from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import Welcome from './components/Welcome';
 
 function App() {
   return (
-    <div>
+    <main className="bg-[var(--color-custom-dark)]">
       <NavBar />
       <StartingScreen />
-      {/* <Welcome />
-      <Skills />
+      <Welcome />
+      {/* <Skills />
       <Projects />
       <About />
       <Contact />
       <Footer /> */}
-    </div>
+    </main>
   );
 }
 
--- AhmedCodes/src/components/StartingScreen.tsx
+++ AhmedCodes/src/components/StartingScreen.tsx
@@ -1,9 +1,10 @@
 import { GlobeAltIcon, CommandLineIcon } from '@heroicons/react/24/outline';
+import DownArrow from './arrows/DownArrow';
 
 const StartingScreen: React.FC = () => {
   return (
-    <section className="min-h-screen pt-28 flex flex-col items-center justify-center bg-[var(--color-custom-dark)] text-white font-mono select-none px-4">
-      <div className="flex items-center gap-6 text-center my-6">
+    <section id="home" className="min-h-screen pt-28 flex flex-col items-center justify-center text-white font-mono select-none px-4 relative">
+      <div className="flex items-center gap-6 text-center">
         <h1>
           <div className="text-[var(--color-custom-yellow)] font-normal text-[6rem] md:text-[8rem] lg:text-[9rem] animate-fade-in-up-scale-color">
             Ahmed
@@ -20,6 +21,7 @@ const StartingScreen: React.FC = () => {
         </p>
         <CommandLineIcon className="w-12 h-12 text-[var(--color-custom-yellow)] animate-icon-pulse translate-y-[0.25rem]" />
       </div>
+      <DownArrow sectionId="welcome" />
     </section>
   );
 };
