b0b40e51463dc6fcee9c96836a73633c47cb305b
Ahmed Alaa <1808283@eng.asu.edu.eg>
2025-05-01 18:54:46 +0300

    Fixed App.tsx

--- AhmedCodes/src/App.tsx
+++ AhmedCodes/src/App.tsx
@@ -1,18 +1,18 @@
 import NavBar from './components/NavBar';
 import StartingScreen from './components/StartingScreen';
-import Welcome from './components/Welcome';
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
+// import Welcome from './components/Welcome';
+// import About from './components/About';
+// import Contact from './components/Contact';
+// import Footer from './components/Footer';
+// import Projects from './components/Projects';
+// import Skills from './components/Skills';
 
 function App() {
   return (
     <main className="bg-[var(--color-custom-dark)]">
       <NavBar />
       <StartingScreen />
-      <Welcome />
+      {/* <Welcome /> */}
       {/* <Skills />
       <Projects />
       <About />
