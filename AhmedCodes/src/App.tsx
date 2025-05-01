2a144e180e8b922351e0c941f07ac8093fef84af
Ahmed Alaa <1808283@eng.asu.edu.eg>
2025-05-01 18:29:21 +0300

    Removed unneceesary code

--- AhmedCodes/src/App.tsx
+++ AhmedCodes/src/App.tsx
@@ -1,11 +1,11 @@
 import NavBar from './components/NavBar';
 import StartingScreen from './components/StartingScreen';
 import Welcome from './components/Welcome';
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
+// import About from './components/About';
+// import Contact from './components/Contact';
+// import Footer from './components/Footer';
+// import Projects from './components/Projects';
+// import Skills from './components/Skills';
 
 function App() {
   return (
